cbba70e6f9bd75b278a1d74277e4664f1b9b26f7
ju0-0913 <juyeong_0913@naver.com>
2023-02-05 16:08:19 +0900

    save() 추가

--- backend/src/main/java/com/inti/nosily_coach/domain/Eat/repository/EatRepository.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Eat/repository/EatRepository.java
@@ -2,6 +2,11 @@ package com.inti.nosily_coach.domain.Eat.repository;
 
 import com.inti.nosily_coach.domain.Eat.model.Eat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor; // Querydsl을 이용하기 위해 추가
 
-public interface EatRepository extends JpaRepository<Eat, Long>, EatRepositoryCustom {
+public interface EatRepository extends JpaRepository<Eat, Long>, EatRepositoryCustom, QuerydslPredicateExecutor<Eat> {
+    @Override
+    Eat save(Eat eat);
+
+    
 }
