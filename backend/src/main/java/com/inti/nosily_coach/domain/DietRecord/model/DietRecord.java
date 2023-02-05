1d0f0f945d6b67a0ae97ee40455b7c9735a9e17b
ju0-0913 <juyeong_0913@naver.com>
2023-02-05 16:06:40 +0900

    엔티티 수정

--- backend/src/main/java/com/inti/nosily_coach/domain/DietRecord/model/DietRecord.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/DietRecord/model/DietRecord.java
@@ -4,21 +4,21 @@ import com.inti.nosily_coach.domain.Eat.model.Eat;
 import com.inti.nosily_coach.domain.Member.model.Member;
 import com.inti.nosily_coach.domain.common.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@ToString
 public class DietRecord extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
-    private Member member;
+    private Member member; // 회원 아이디
 
     @OneToMany(mappedBy = "dietRecord", cascade = CascadeType.ALL)
     private List<Eat> eats = new ArrayList<>();
@@ -27,7 +27,7 @@ public class DietRecord extends BaseEntity {
     private String time; // 아침, 점심, 저녁, 간식
 
     @Column(length = 300)
-    private String memo;
+    private String memo; // 메모
 
     @Builder(access = AccessLevel.PRIVATE)
     private DietRecord(Member member, String time, String memo) {
--- backend/src/main/java/com/inti/nosily_coach/domain/Eat/model/Eat.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Eat/model/Eat.java
@@ -3,26 +3,26 @@ package com.inti.nosily_coach.domain.Eat.model;
 import com.inti.nosily_coach.domain.DietRecord.model.DietRecord;
 import com.inti.nosily_coach.domain.common.BaseEntity;
 import com.inti.nosily_coach.domain.food.model.Food;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Eat extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dietRecord_id")
-    private DietRecord dietRecord;
+    private DietRecord dietRecord; // 식단기록 id
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "food_id")
-    private Food food;
+    private Food food; // 음식 id
+
+    @Column
+    private Long intake; // 섭취량
 
     @Builder(access = AccessLevel.PRIVATE)
     private Eat(DietRecord dietRecord, Food food) {
