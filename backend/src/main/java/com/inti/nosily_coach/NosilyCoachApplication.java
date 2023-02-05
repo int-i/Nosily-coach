d909db4cdf89a7ac663aa937932b73147803f02b
ju0-0913 <juyeong_0913@naver.com>
2023-02-05 16:06:14 +0900

    JPA를 이용하면서 AuditingEntityListener를 활성화시키기 위해 EnableJpaAuditing 어노테이션 추가

--- backend/src/main/java/com/inti/nosily_coach/NosilyCoachApplication.java
+++ backend/src/main/java/com/inti/nosily_coach/NosilyCoachApplication.java
@@ -2,8 +2,11 @@ package com.inti.nosily_coach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA를 이용하면서 AuditingEntityListener를 활성화시키기 위해 설정 추가
 public class NosilyCoachApplication {
 
 	public static void main(String[] args) {
