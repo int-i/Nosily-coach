4e1eaca4bec10b9b3481635380a9df0bac8446d3
ju0-0913 <juyeong_0913@naver.com>
2023-02-07 10:16:04 +0900

    Eat Controller 구현

--- backend/src/main/java/com/inti/nosily_coach/domain/Eat/Controller/EatController.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Eat/Controller/EatController.java
@@ -1,15 +1,45 @@
 package com.inti.nosily_coach.domain.Eat.Controller;
 
+import com.inti.nosily_coach.domain.DietRecord.Service.DietRecordService;
+import com.inti.nosily_coach.domain.DietRecord.model.DietRecord;
+import com.inti.nosily_coach.domain.DietRecord.repository.DietRecordRepository;
+import com.inti.nosily_coach.domain.Eat.DTO.DtoEat;
+import com.inti.nosily_coach.domain.Eat.DTO.ModifyEat;
+import com.inti.nosily_coach.domain.Eat.DTO.RequestEat;
+import com.inti.nosily_coach.domain.Eat.DTO.ResponseEat;
+import com.inti.nosily_coach.domain.Eat.Service.EatService;
+import com.inti.nosily_coach.domain.food.model.Food;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/api/Eat")
+@RequiredArgsConstructor
 public class EatController {
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list() { // 목록을 보여주는 메서드
-        return "/api/list"; // /api/list URL 반환하기
+    private final EatService eatService;
+    private final DietRecordService dietRecordService;
+    private final DietRecordRepository dietRecordRepository;
+
+    @GetMapping("/view")
+    public List<DtoEat> viewEatApi(Long foodId, @PageableDefault(size = 5) Pageable pageable) {
+        List<DtoEat> eat = eatService.viewEat(foodId, pageable);
+        return eat;
+    }
+
+    @PostMapping("/create")
+    public ResponseEat createEatApi(DietRecord dietRecord, Food food, @RequestBody RequestEat requestEat) {
+        return eatService.createEat(dietRecord, food, requestEat);
+    }
+
+    @PostMapping("/modify")
+    public ModifyEat modifyEat(Long memberId, @PathVariable("food") Food food, @PathVariable("DietRecord") Long DietRecordId, @RequestBody RequestEat requestEat) {
+        return eatService.modifyEat(memberId, food, DietRecordId, requestEat);
     }
 }
